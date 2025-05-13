c52d08bb8ab66d78bc9e0c49c91612eead6f1e73
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-05-13 10:36:44 +0200

    formulaire

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,3 +16,14 @@ def club():
 @app.route("/apprendre")
 def apprendre():
     return render_template("termes_echec.html")
+
+
+@app.route("/sinscrire", methods=["GET", "POST"])
+def inscription():
+    if request.method == "POST":
+        # getting input with name = fname in HTML form
+        first_name = request.form.get("fname")
+        # getting input with name = lname in HTML form
+        last_name = request.form.get("lname")
+        return "Your name is " + first_name + last_name
+    return render_template("form.html")
