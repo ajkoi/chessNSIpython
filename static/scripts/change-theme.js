2840e9c0f45fae6aa546510b41ba15fa7dd0bb2a
Tteocarre <perre.matteo@gmail.com>
2025-05-19 22:25:24 +0200

    g fait des modifs sur main.py (routr/paiement) et aussi g rajouter des entree dans form2.html, aussi j'ai mis une commande qu'il faudra que l'on vérifie ensemble sur creditNSI, et il faudra que tu me dises comment relier la page creditNSI a form2

--- static/scripts/change-theme.js
+++ static/scripts/change-theme.js
@@ -2,9 +2,9 @@
     my_img.addEventListener('click', () => {
         document.body.classList.toggle('light_theme');
         const my_img_now = my_img.getAttribute('src');
-        if (my_img_now != 'static/images/menu/dark_theme_button.png') {
-            my_img.setAttribute('src', 'static/images/menu/dark_theme_button.png');
+        if (my_img_now != '../images/menu/dark_theme_button.png') {
+            my_img.setAttribute('src', '../images/menu/dark_theme_button.png');
         } else {
-            my_img.setAttribute('src', 'static/images/menu/light_theme_button.png');
+            my_img.setAttribute('src', '../images/menu/light_theme_button.png');
         }
     });
