36c33c64bf0b087f33c304c8827861dcf621a164
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-05-21 00:11:35 +0200

    echecs

--- static/scripts/Echecs.js
+++ static/scripts/Echecs.js
@@ -1,176 +1,4 @@
-// // TODO faire le jeu :)
-
-// let chessboard = [
-//     ['T', 'N', 'B', 'K', 'Q', "B", "K", "T"],
-//     ['P', 'P', 'P', 'P', 'P', "P", "P", "P"],
-//     ['', '', '', '', '', '', '', ''],
-//     ['', '', '', '', '', '', '', ''],
-//     ['', '', '', '', '', '', '', ''],
-//     ['', '', '', '', '', '', '', ''],
-//     ['p', 'p', 'p', 'p', 'p', "p", "p", "p"],
-//     ['t', 'n', 'b', 'k', 'q', "b", "k", "t"],
-// ]
-
-// let player = 0
-// let tour = 1
-// let canPlayer1Roque = true
-// let canPayer2Roque = true
-
-// function jouerCoup (startSquare, endSquare) {
-//     /*
-//     * startSquare: tuple(int, int)
-//     * endSquare: tuple(int, int) */
-//     if (isPlayable(startSquare, endSquare)) {
-//         if (isItaRoque(startSquare, endSquare)) {
-//             // ROQUE PUTAIN DE MERDE
-//         } else {
-//             chessboard[endSquare[0]][endSquare[1]] = chessboard[startSquare[0]][startSquare[1]]
-//             chessboard[startSquare[0]][startSquare[1]] = ''
-//         }
-//     }
-// }
-
-// function isPlayable (startSquare, endSquare) {
-//     return true
-// }
-
-// function isPlayableKing (startSquare, endSquare) {
-//     if ((abs(startSquare[1]-endSquare[1]) <= 1) && (abs(startSquare[0]-endSquare[0]) <= 1)) {
-//         return true
-//     }
-//     else {
-//         return false
-//     }
-    
-// }
-
-// function isPlayablePawn (startSquare, endSquare) { //TODO : faire la prise en passant et clean un peu ce code
-//     if (abs(startSquare[1]-endSquare[1] === 1) && chessboard[endSquare[0][endSquare[1]]] === '' && startSquare[0]===endSquare[0]) {
-//         return true
-//     }
-//     else if ((abs(startSquare[1]-endSquare[1] === 2
-//         && startSquare[0]===endSquare[0]))
-//         && (chessboard[startSquare[0][startSquare[1]]] === 'P' && startSquare[1] === 2 && chessboard[startSquare[0]][startSquare[1]+1] === '' )
-//         || (chessboard[startSquare[0][startSquare[1]]] === 'p' && startSquare[1] === 7 && chessboard[startSquare[0]][startSquare[1]-1] === '' )
-//         && chessboard[endSquare[0][endSquare[1]]] === '')
-//     {
-//           return true
-//     }
-//     else if (chessboard[startSquare[0][startSquare[1]]] === 'P') { // prise blancs
-//         if (abs(startSquare[0]-endSquare[0]) === 1 && abs(startSquare[1]-endSquare[1]) === -1 && !(chessboard[endSquare[0][endSquare[1]]] === '')) {
-//             return true
-//         }
-//     }
-//     else if (chessboard[startSquare[0][startSquare[1]]] === 'p') { // prise
-//         if (abs(startSquare[0]-endSquare[0]) === 1 && abs(startSquare[1]-endSquare[1]) === 1 && !(chessboard[endSquare[0][endSquare[1]]] === '')) {
-//             return true
-//         }
-//     }
-// }
-
-// function isPlayableKnight (startSquare, endSquare) {
-//     let Squares = [
-//                 [startSquare[0]-1, startSquare[1]+2],[startSquare[0]+1, startSquare[1]+2],
-//                 [startSquare[0]+2, startSquare[1]+1],[startSquare[0]+2, startSquare[1]-1],
-//                 [startSquare[0]-2, startSquare[1]+1],[startSquare[0]-2, startSquare[1]-1],
-//                 [startSquare[0]-1, startSquare[1]-2],[startSquare[0]+1, startSquare[1]-2],
-//             ]
-//     if (endSquare in Squares) {
-//         return true
-//     }
-//     else {
-//         return false
-//     }
-// }
-
-// function isPlayableRook (startSquare, endSquare) {
-//     let Squares = []
-//     for (let i = startSquare[0]; i < 8 ;i++) { // vers la droite
-//         if (!chessboard[i][startSquare[1]] === '') {break}
-//         else {
-//             Squares.push([i, startSquare[1]])
-//         }
-//     }
-//     for (let i = startSquare[0]; i > 0 ;i--) { // vers la gauche
-//         if (!chessboard[i][startSquare[1]] === '') {break}
-//         else {
-//             Squares.push([i, startSquare[1]])
-//         }
-//     }
-//     for (let i = startSquare[1]; i < 8 ;i++) {
-//         if (!chessboard[startSquare[0]][i] === '') {break}
-//         else {
-//             Squares.push([startSquare[0], i])
-//         }
-//     }
-//     for (let i = startSquare[1]; i > 0 ;i--) {
-//         if (!chessboard[startSquare[0]][i] === '') {break}
-//         else {
-//             Squares.push([startSquare[0], i])
-//         }
-//     }
-//     console.log(Squares)
-//     if (endSquare in Squares) {
-//         return true
-//     }
-//     else {
-//         return false
-//     }
-// }
-
-// function abs(number){
-//     if (number>0) {
-//         return number
-//     }
-//     else {
-//         return -number
-//     }
-
-// }
-
-// function isItaRoque(startSquare, endSquare) {
-//     if (!(chessboard[startSquare[0]][startSquare[1]] === 'R' || chessboard[startSquare[0]][startSquare[1]] === 'r' )) {
-//         return false
-//     }
-//     else if (abs(startSquare[0]-endSquare[0]) === 2) {
-//         return true
-//     }
-
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-
+// 
 let player = 0
 let tour = 1
 let canPlayer1Roque = true
@@ -198,6 +26,7 @@ let chessboard = [
     "PPPPPPPP",
     "RNBQKBNR",
 ]
+let coups = [[[0, 0], [1, 1, 1]]]
 
 function playableRook (square, player) {
     let squares = []
@@ -471,15 +300,15 @@ function playableKing(square, player) {
         for (let i = -1; i<=1; i++) {
             for (let j = -1; j<=1; j++) {
             let checked_square = [square[0]+i, square[1]+j]
-            if (checked_square[0]===0 || checked_square[0 === 7 || checked_square[1] === 0 || checked_square[1] === 7] || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
+            if (checked_square[0]<0 || checked_square[0] > 7 || checked_square[1] < 0 || checked_square[1] > 7 || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
                 continue
             }
-            if (isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
-                break
+            else if (isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
+                continue
             }
             else if (!isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')){
                 squares.push(checked_square)
-                break
+                continue
             }
             else {
                 squares.push(checked_square)
@@ -491,20 +320,122 @@ function playableKing(square, player) {
         for (let i = -1; i<=1; i++) {
             for (let j = -1; j<=1; j++) {
             let checked_square = [square[0]+i, square[1]+j]
-            // if (checked_square[0]===0 || checked_square[0 === 7 || checked_square[1] === 0 || checked_square[1] === 7] || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
-            //     continue
-            // }
-            if (!isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
-                break
+            if (checked_square[0]<0 || checked_square[0] > 7 || checked_square[1] < 0 || checked_square[1] > [7] || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
+                continue
+            }
+            else if (!isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
+                continue
             }
             else if (isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')){
                 squares.push(checked_square)
-                break
+                continue
             }
             else {
                 squares.push(checked_square)
+            // }
             }
+        }
+    }
+}
+
+    return squares
+}
+
+
+function playableKnight(square, player) {
+    let possible_squares = [[2, -1], [2, 1], [1, 2], [-1, 2], [1, -2], [-1, -2], [-2, 1], [-2, -1]]
+    let squares = []
+    if (player===0) {
+        for (let i=0; i<possible_squares.length; i++) {
+            let checked_square = [square[0]+possible_squares[i][0], square[1]+possible_squares[i][1]]
+            if (checked_square[0]<0 || checked_square[0] > 7 || checked_square[1] < 0 || checked_square[1] > [7] || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
+                continue
             }
+            else if (isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
+                continue
+            }
+            else if (!isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')){
+                squares.push(checked_square)
+                continue
+            }
+            else {
+                squares.push(checked_square)
+            // }
+            }
+
+        }
+    }
+    else if (player===1) {
+        for (var i=0; i<possible_squares.length; i++) {
+            let checked_square = [square[0]+possible_squares[i][0], square[1]+possible_squares[i][1]]
+            if (checked_square[0]<0 || checked_square[0] > 7 || checked_square[1] < 0 || checked_square[1] > [7] || (checked_square[0]===square[0] && checked_square[1]===square[1])) {
+                continue
+            }
+            else if (!isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')) {
+                continue
+            }
+            else if (isUpper(chessboard[checked_square[0]][checked_square[1]]) && !(chessboard[checked_square[0]][checked_square[1]]===' ')){
+                squares.push(checked_square)
+                continue
+            }
+            else {
+                squares.push(checked_square)
+            // }
+            }
+
+        }
+    }
+    return squares
+}
+
+function playablePawn(square, player) {
+    let squares = []
+    if (player===0) {
+        if (chessboard[square[0]-1][square[1]]===' ') {
+           squares.push([square[0]-1, square[1]]) 
+        }
+        if (chessboard[square[0]-2][square[1]]===' ' && square[0]===6) {
+           squares.push([square[0]-2, square[1], 1]) 
+        }
+        if (!isUpper(chessboard[square[0]-1][square[1]+1])) { // prise
+            squares.push([square[0]-1, square[1]+1])
+        }
+        if (!isUpper(chessboard[square[0]-1][square[1]-1])) {
+            squares.push([square[0]-1, square[1]-1])
+        }
+        if (chessboard[square[0]][square[1]-1]==='p') { // en passant
+            if (coups[0][1].length===3) {
+                squares.push([square[0]-1, square[1]-1])
+            } 
+        }
+        if (chessboard[square[0]][square[1]+1]==='p') {
+            if (coups[0][1].length===3) {
+                squares.push([square[0]-1, square[1]+1])
+            } 
+        }
+    }
+    else if (player===1) {
+        if (chessboard[square[0]+1][square[1]]===' ') {
+           squares.push([square[0]+1, square[1]]) 
+        }
+        if (chessboard[square[0]+2][square[1]]===' ' && square[0]===1) {
+           squares.push([square[0]+2, square[1], 1]) 
+        }
+        if (!isUpper(chessboard[square[0]+1][square[1]+1])) {
+            squares.push([square[0]+1, square[1]+1])
+        }
+        if (!isUpper(chessboard[square[0]+1][square[1]-1])) {
+            squares.push([square[0]+1, square[1]-1])
+        }
+        if (chessboard[square[0]][square[1]+1]==='P') {
+            if (coups[0][1].length===3) {
+                squares.push([square[0]+1, square[1]-1])
+            } 
+        }
+        if (chessboard[square[0]][square[1]+1]==='P') {
+            if (coups[0][1].length===3) {
+                squares.push([square[0]+1, square[1]+1])
+            } 
         }
     }
     return squares
