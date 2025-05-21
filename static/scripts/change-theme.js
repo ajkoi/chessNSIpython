f1c9cc8f0198317a1f08add1ea7fa943d96dedf4
Tteocarre <perre.matteo@gmail.com>
2025-05-21 11:46:46 +0200

    caca

--- static/scripts/change-theme.js
+++ static/scripts/change-theme.js
@@ -2,9 +2,10 @@
     my_img.addEventListener('click', () => {
         document.body.classList.toggle('light_theme');
         const my_img_now = my_img.getAttribute('src');
-        if (my_img_now != '../images/menu/dark_theme_button.png') {
-            my_img.setAttribute('src', '../images/menu/dark_theme_button.png');
+        if (my_img_now != '/images/menu/dark_theme_button.png') {
+            my_img.setAttribute('src', src="{{ url_for('static', filename='images/menu/light_theme_button.png') }}");
+            console.log("ccava")
         } else {
-            my_img.setAttribute('src', '../images/menu/light_theme_button.png');
+            my_img.setAttribute('src', src="{{ url_for('static', filename='images/menu/dark_theme_button.png') }}");
         }
     });
