c85c5ba8ec61064b6c88dc73458129aa1093a6ea
Tteocarre <perre.matteo@gmail.com>
2025-05-21 19:53:30 +0200

    r

--- static/scripts/change-theme.js
+++ static/scripts/change-theme.js
@@ -2,7 +2,8 @@
     my_img.addEventListener('click', () => {
         document.body.classList.toggle('light_theme');
         const my_img_now = my_img.getAttribute('src');
-        if (my_img_now != '/images/menu/dark_theme_button.png') {
+        console.log(my_img_now)
+        if (my_img_now != '../images/menu/dark_theme_button.png') {
             my_img.setAttribute('src', src="{{ url_for('static', filename='images/menu/light_theme_button.png') }}");
             console.log("ccava")
         } else {
